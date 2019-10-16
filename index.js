214a9eb2a89a6b9117852998acbaa97c6df612ad
Sabrina Becker <sbecka8@gmail.com>
2019-10-15 23:42:06 -0400

    added event listeners for check and delete buttons on old and new shopping items

--- index.js
+++ index.js
@@ -25,20 +25,15 @@ $(function () {
     }
     );
 
-    
+    //event delegation
+    $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+    });
+
+    $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+        $(this).closest("li").remove();
+    });
 
 
 
 });
-
-//<li>
-//<span class="shopping-item">apples</span>
-//<div class="shopping-item-controls">
-  //<button class="shopping-item-toggle">
-    //<span class="button-label">check</span>
-  //</button>
-  //<button class="shopping-item-delete">
-    //<span class="button-label">delete</span>
-  //</button>
-//</div>
-//</li>
